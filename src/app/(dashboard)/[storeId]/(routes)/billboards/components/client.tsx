b6f714f64ea40c6414b69296dcf124ec2186d76d
Sultan-Medderetain <freelancerahad222@gmail.com>
2024-04-23 12:16:36 -0600

    refactor: middleware and reusable api list component

--- src/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -32,6 +33,12 @@ export const BillboardClient = ({ data }: BillboardClientProps) => {
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label" />
+      <Separator />
+      <Heading
+        title="API"
+        description="Make API request for billboards (GET request only)."
+      />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
